336a2e30353d3412232e6b3a335f8acd3916b7b3
cuining <1015597172@qq.com>
2024-12-06 23:41:47 +0800

    Mine
    Create Mine Android Project

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+        mavenLocal().apply { url = uri("$rootDir\\local_maven") }
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        mavenLocal().apply { url = uri("$rootDir\\local_maven") }
+    }
+}
+
+rootProject.name = "MineAndroid"
+include(":app")
+ 

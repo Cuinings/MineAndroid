c1e42c325dbf530bcef772848000e8f51c18b86e
Cuinings <>
2024-12-10 11:20:03 +0800

    添加节流函数库

---
+++ Library/Common/Throttle/src/main/java/com/cn/library/common/throttle/Throttle.kt
@@ -0,0 +1,23 @@
+package com.cn.library.common.throttle
+
+object Throttle {
+    private val timerMap = HashMap<String, Long>()
+    fun throttle(duration: Long = 500L, callBack: (() -> Unit)? = null, task:() -> Unit) {
+        val currentTime = System.currentTimeMillis()
+        if (((currentTime - (timerMap[task.javaClass.toString()] ?:0))) > duration) {
+            task.invoke()
+            timerMap[task.javaClass.toString()] = System.currentTimeMillis()
+        } else {
+            callBack?.invoke()
+        }
+    }
+
+    fun throttle(duration: Long = 500L, callBack: (() -> Unit)? = null, last: Long, task:() -> Unit) {
+        val currentTime = System.currentTimeMillis()
+        if (currentTime - last > duration) {
+            task.invoke()
+        } else {
+            callBack?.invoke()
+        }
+    }
+}

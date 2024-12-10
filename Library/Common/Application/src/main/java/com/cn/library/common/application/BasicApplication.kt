91f48746dce0b1e0d503d8719ab6e12baefb097e
Cuinings <>
2024-12-10 11:25:26 +0800

    添加Application封装库

---
+++ Library/Common/Application/src/main/java/com/cn/library/common/application/BasicApplication.kt
@@ -0,0 +1,33 @@
+package com.cn.library.common.application
+
+import android.app.Application
+import android.content.Context
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelStore
+import androidx.lifecycle.ViewModelStoreOwner
+import com.cn.library.common.application.ApplicationContextExt.context
+
+/**
+ * @Author: CuiNing
+ * @Time: 2024/10/15 9:04
+ * @Description:
+ */
+abstract class BasicApplication: Application(), ViewModelStoreOwner {
+
+    protected val TAG: String by lazy { this.javaClass.simpleName }
+
+    private val mAppViewModelStore: ViewModelStore = ViewModelStore()
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        base?.let { context = it }
+    }
+
+    val viewmodelProvider: ViewModelProvider get() = ViewModelProvider(this, appFactory)
+
+    private val appFactory: ViewModelProvider.Factory get() = ViewModelProvider.AndroidViewModelFactory.getInstance(this)
+
+    override val viewModelStore: ViewModelStore
+        get() = mAppViewModelStore
+
+}

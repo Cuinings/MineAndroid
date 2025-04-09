f64b7864e8bfa58fa032f5dc903f65d1ace60f3a
Cuining <19723046+Cuinings@users.noreply.github.com>
2025-04-09 17:39:04 +0800

    Sync

--- Mine/WanAndroid/src/main/java/com/cn/mine/wan/android/app/page/main/MainActivityViewModel.kt
+++ Mine/WanAndroid/src/main/java/com/cn/mine/wan/android/app/page/main/MainActivityViewModel.kt
@@ -3,14 +3,12 @@ package com.cn.mine.wan.android.app.page.main
 import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewModelScope
-import com.cn.mine.wan.android.net.WanAndroidAPI
-import com.cn.mine.wan.android.data.entity.ArticleEntity
-import com.cn.mine.wan.android.data.entity.CommonData
-import com.cn.mine.wan.android.data.entity.CommonPageData
+import com.cn.library.common.activity.BasicActivity.Companion.TAG
 import com.cn.mine.wan.android.data.entity.result
+import com.cn.mine.wan.android.net.WanAndroidAPI
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -20,13 +18,22 @@ class MainActivityViewModel @Inject constructor(): ViewModel() {
     @Inject
     lateinit var wanAndroidAPI: WanAndroidAPI
 
-    fun doSomething() {
-        Log.d(MainActivityViewModel::class.simpleName, "doSomething: wanAndroidAPI -> $wanAndroidAPI")
-    }
-
     init { viewModelScope.launch {
         Log.d(MainActivityViewModel::class.simpleName, "init: ")
     } }
+
+    fun onRefresh() {
+        viewModelScope.launch {
+            wanAndroidAPI.article(0).result({
+                Log.d(MainActivityViewModel::class.simpleName, "article: $it")
+//                binding.articleView.addArticle(it)
+            }) {
+//                Toast.makeText(this@MainActivity, it, Toast.LENGTH_SHORT).show()
+//                binding.articleView.isRefreshing = false
+            }
+            Log.d(TAG, "onCreate: onRefresh")
+        }
+    }
 
 
 }

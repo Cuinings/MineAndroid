e5ef010dc71df467e3a93558861fafb636656a08
Cuining <19723046+Cuinings@users.noreply.github.com>
2025-04-10 17:30:26 +0800

    Sync

--- Mine/Wanandroid-compose/src/main/java/com/cn/mine/wan/android/compose/MainActivity.kt
+++ Mine/Wanandroid-compose/src/main/java/com/cn/mine/wan/android/compose/MainActivity.kt
@@ -6,13 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.cn.mine.wan.android.compose.ui.MainEntrance
 import com.cn.mine.wan.android.compose.ui.theme.MineAndroidTheme
 
+@ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,28 +23,17 @@ class MainActivity : ComponentActivity() {
         setContent {
             MineAndroidTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MainEntrance(Modifier.padding(innerPadding).fillMaxSize())
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MineAndroidTheme {
-        Greeting("Android")
+//        MainEntrance()
     }
 }
---
+++ Mine/Wanandroid-compose/src/main/java/com/cn/mine/wan/android/compose/ui/MainEntrance.kt
@@ -0,0 +1,24 @@
+package com.cn.mine.wan.android.compose.ui
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+/**
+ * @Author:         cn
+ * @Date:           2025/4/10 16:39
+ * @Description:
+ */
+@ExperimentalMaterial3Api
+@Composable
+fun MainEntrance(modifier: Modifier = Modifier) {
+    PullToRefreshBox(false, onRefresh = {
+
+    }, modifier = modifier) {
+        LazyColumn(modifier = modifier) {
+
+        }
+    }
+}

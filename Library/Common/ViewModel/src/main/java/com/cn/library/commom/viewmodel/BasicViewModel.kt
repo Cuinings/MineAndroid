84304e87c4df2f86efe874c23ee67e832e341992
Cuining <19723046+Cuinings@users.noreply.github.com>
2025-08-12 14:55:58 +0800

    ViewModel module

--- Library/Common/ViewModel/src/main/java/com/cn/library/commom/viewmodel/BasicViewModel.kt
+++ Library/Common/ViewModel/src/main/java/com/cn/library/commom/viewmodel/BasicViewModel.kt
@@ -7,7 +7,9 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.update
@@ -22,21 +24,21 @@ import kotlinx.coroutines.launch
 interface UIState
 @Keep
 interface UIEvent
-abstract class BasicViewModel<S: UIState, I: UIEvent>: ViewModel() {
+abstract class BasicViewModel<S: UIState, E: UIEvent>: ViewModel() {
 
     companion object {
         lateinit var TAG: String
     }
 
     private val _uiStateFlow = MutableStateFlow(this.initUIState())
-    private val _uiIntentFlow = Channel<I>()
+    private val _uiIntentFlow = Channel<E>()
 
-    val uiStateFlow: StateFlow<S> = _uiStateFlow.asStateFlow()
-    val uiIntentFlow: Flow<I> = _uiIntentFlow.receiveAsFlow()
+    val uiStateFlow: SharedFlow<S> = _uiStateFlow.asStateFlow()
+    val uiIntentFlow: Flow<E> = _uiIntentFlow.receiveAsFlow()
 
 
-    fun sendUiState(copy: S.() -> S) = _uiStateFlow.update { copy(_uiStateFlow.value) }//.apply { Log.d(TAG, "sendUiState: ${_uiStateFlow.value}") }
-    fun sendUIIntent(intent: I) = viewModelScope.launch { _uiIntentFlow.send(intent) }
+    fun sendUiState(copy: S.() -> S) = _uiStateFlow.update { copy(_uiStateFlow.value.copy()) }
+    fun sendUIIntent(intent: E) = viewModelScope.launch { _uiIntentFlow.send(intent) }
 
     init {
         TAG = javaClass.simpleName
@@ -51,7 +53,7 @@ abstract class BasicViewModel<S: UIState, I: UIEvent>: ViewModel() {
     /**
      * 处理意图
      */
-    abstract fun handleEvent(event: I)
+    abstract fun handleEvent(event: E)
 
 
 }

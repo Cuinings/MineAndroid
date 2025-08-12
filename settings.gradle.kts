2c48eb82e7daca6bc69c34b66c7ca44ab28931d5
Cuining <19723046+Cuinings@users.noreply.github.com>
2025-08-12 15:01:06 +0800

    other

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,5 @@
 pluginManagement {
+    includeBuild("build-logic")
     repositories {
         maven { url = uri("https://maven.aliyun.com/repository/public") }
         maven { url = uri("https://maven.aliyun.com/repository/google") }
@@ -35,34 +36,42 @@ dependencyResolutionManagement {
 
 rootProject.name = "MineAndroid"
 
+//include(":TP:Launcher")
+//include(":TP:Launcher:Model")
+//include(":TP:Launcher:Event")
+//include(":TP:Launcher:Repository")
+
 include(":Sample:Test")
-include(":Sample:Hilt")
-include(":Sample:Design-Model")
+//include(":Sample:Hilt")
 
-include(":Mine:WanAndroid:App")
-include(":Mine:WanAndroid:Events")
-include(":Mine:WanAndroid:Repository")
-include(":Mine:WanAndroid:Overlay")
+//include(":Sample:Design-Model")
+//include(":Mine:WanAndroid:App")
+//include(":Mine:WanAndroid:Events")
+//include(":Mine:WanAndroid:Repository")
+//include(":Mine:WanAndroid:Overlay")
+//include(":Mine:Wanandroid-Compose:App")
 
+//include(":Mine:Wanandroid-Compose:core:data")
 include(":Library:Remote:Andlinker")
 include(":Library:Remote:Msg:Subscriber:Annotation")
 include(":Library:Remote:Msg:Subscriber:Processor")
 include(":Library:Remote:Msg:Router:Client")
-include(":Library:Remote:Msg:Router:Service")
 
+include(":Library:Remote:Msg:Router:Service")
 include(":Library:Common:Application")
 include(":Library:Common:Activity")
+include(":Library:Common:Fragment")
+
 include(":Library:Common:Service")
 include(":Library:Common:ViewModel")
-
 include(":Library:Common:Popupwindow")
+
 include(":Library:Common:Dpi")
+
 include(":Library:Common:Drawable:DSL")
 
 include(":Library:Common:Color")
-
 include(":Library:Common:EditText")
-
 include(":Library:Common:Flow")
 include(":Library:Common:RecyclerView:Adapter")
 include(":Library:Utils:Throttle")
@@ -72,3 +81,4 @@ include(":Library:Utils:IP")
 include(":Library:Utils:Gson")
 include(":Library:Utils:NetWork")
 include(":Library:Common:WebView")
+include(":Library:Common:MicEnergyView")
